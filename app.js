219b8ee04931e0848086919e3ca6d368930dc5de
Andrew Litten <andrew.litten@gmail.com>
2024-11-23 16:09:31 -0800

    added start files and completed the javascript for functionality

--- app.js
+++ app.js
@@ -29,3 +29,64 @@ const reviews = [
       text: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
     },
   ];
+
+  const img = document.getElementById("person-img")
+  const author = document.getElementById("author")
+  const job = document.getElementById("job")
+  const info = document.getElementById("info")
+
+const prevBtn = document.querySelector('.prev-btn')
+const nextBtn = document.querySelector('.next-btn')
+const randomBtn = document.querySelector('.random-btn')
+
+//set starting item
+
+let currentItem = 0;
+
+// load initial item
+
+window.addEventListener('DOMContentLoaded', function(){
+    showPerson(currentItem);
+})
+const buttons = document.querySelectorAll('.btn')
+
+// show person based on item
+function showPerson(person){
+  const item = reviews[person]
+    img.src = item.img;
+    author.textContent = item.name;
+    job.textContent = item.job;
+    info.textContent = item.text;
+}
+
+// event listeners for next button 
+nextBtn.addEventListener('click', function(){
+  currentItem++
+  if (currentItem > reviews.length -1){
+    currentItem = 0;
+    
+  }
+  showPerson(currentItem)
+  
+})
+// event listeners for back button 
+prevBtn.addEventListener('click', function(){
+  currentItem--
+  if (currentItem < 0){
+    currentItem = reviews.length -1;
+  }
+  showPerson(currentItem)
+})
+
+// event listener for showing random person
+randomBtn.addEventListener('click', function(){
+    currentItem = Math.floor(Math.random() * reviews.length)
+    showPerson(currentItem)
+
+})
+
+
+
+
+
+
